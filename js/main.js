fdf090f54e8e15dbc389c5b06bbd09476930d3a9
Tornike Zhizhiashvili <t.zhizhiashvili@adjarabet.com>
2021-07-29 02:06:46 +0400

    add guees number js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,70 @@
-'use strict'
+
+
+var check = document.querySelector(".check")
+var messageBox = document.querySelector(".message").textContent
+let winNumber = Math.floor(Math.random() * 20 + 1);
+
+let score = 20;
+let hightScore = 0;
+
+check.addEventListener("click",function(){
+  
+    let guessNumber = Number(document.querySelector(".guess").value)
+    
+    function win(){
+
+        document.querySelector(".number").innerText = winNumber;
+        document.querySelector("body").style.background = "#60b347";
+        document.querySelector(".message").innerText = "You Win";
+        
+        if(winNumber > hightScore){
+            hightScore = winNumber
+            document.querySelector(".highscore").innerText = winNumber
+        }
+    }
+
+
+    function wasteTry(){
+        score--;
+        document.querySelector(".score").innerText = score
+
+    }
+
+    if(!guessNumber){
+        document.querySelector(".message").innerText = "no Number!!!"
+    }else {
+        if(guessNumber === winNumber){
+
+            win()
+        }else {
+    
+            wasteTry()
+          
+            if(guessNumber > winNumber) {
+                document.querySelector(".message").innerText = "Too High"
+    
+            }else {
+                document.querySelector(".message").innerText = "Too Low"
+    
+            }
+    
+        }
+    }
+    
+    
+})
+
+
+
+let again = document.querySelector(".again")
+
+again.addEventListener("click",function(){
+
+    document.querySelector(".number").innerText = "?";
+    document.querySelector(".guess").value = " ";
+    winNumber = Math.floor(Math.random() * 10 + 1)
+    document.querySelector("body").style.background = "#222";
+    document.querySelector(".message").innerText = "Start guessing.."
+
+})
+
